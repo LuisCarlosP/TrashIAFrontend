e21afd13a82a0cff6c0a3bb26b7358f664bcdb18
LuisCarlosPicadoRojas <144180800+LuisCarlosP@users.noreply.github.com>
2025-12-08 19:04:01 -0600

    Updated Footer

--- src/components/common/Footer/FooterLinksSection/FooterLinksSection.tsx
+++ src/components/common/Footer/FooterLinksSection/FooterLinksSection.tsx
@@ -18,15 +18,19 @@ export default function FooterLinksSection({
     navMap,
     navScanner
 }: FooterLinksSectionProps) {
+    const scrollToTop = () => {
+        window.scrollTo({ top: 0, behavior: 'smooth' })
+    }
+
     return (
         <div className="footer-section">
             <h3 className="footer-section-title">{linksTitle}</h3>
             <ul className="footer-links">
-                <li><Link to="/">{navHome}</Link></li>
-                <li><Link to="/trashia">{navClassifier}</Link></li>
-                <li><Link to="/map">{navMap}</Link></li>
-                <li><Link to="/scanner">{navScanner}</Link></li>
-                <li><Link to="/terms">{termsLink}</Link></li>
+                <li><Link to="/" onClick={scrollToTop}>{navHome}</Link></li>
+                <li><Link to="/trashia" onClick={scrollToTop}>{navClassifier}</Link></li>
+                <li><Link to="/map" onClick={scrollToTop}>{navMap}</Link></li>
+                <li><Link to="/scanner" onClick={scrollToTop}>{navScanner}</Link></li>
+                <li><Link to="/terms" onClick={scrollToTop}>{termsLink}</Link></li>
             </ul>
         </div>
     )
